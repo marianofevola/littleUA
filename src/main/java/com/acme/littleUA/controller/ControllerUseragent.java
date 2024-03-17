888beb4d4da88bc54a43e7fc8b5f7196d615c494
mariano <fevolamariano@gmail.com>
2024-03-17 01:37:24 +0100

    get list of stored useragents

--- src/main/java/com/acme/littleUA/controller/ControllerUseragent.java
+++ src/main/java/com/acme/littleUA/controller/ControllerUseragent.java
@@ -5,8 +5,11 @@ import com.acme.littleUA.service.useragent.ServiceCrudUseragent;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ControllerUseragent {
@@ -21,5 +24,10 @@ public class ControllerUseragent {
       return ResponseEntity.ok(ua.getUseragent());
     }
     return ResponseEntity.badRequest().build();
+  }
+
+  @GetMapping("/useragents")
+  public ResponseEntity<List<EntityUseragent>> useragentList() {
+    return ResponseEntity.ok(serviceCrudUseragent.find(10).getContent());
   }
 }
